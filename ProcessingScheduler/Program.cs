7b37810c05e2061d6852271e891a8926611659fa
emilroman <emir23us@yahoo.com>
2018-03-21 09:40:47 +0200

    Add project files.

---
+++ ProcessingScheduler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProcessingScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
